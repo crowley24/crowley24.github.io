9f9d7581ac65d508546d9c3f71b2e6a128dfaf52
crowley24 <unclezhenya26@gmail.com>
2025-11-04 21:39:59 +0200

    Update and rename cardify_ver2.1.js to cardify_v2.js

--- cardify_v2.js
+++ cardify_v2.js
@@ -1120,7 +1120,7 @@
       /* Трейлер на фоні справа з 60% прозорістю */      
       .cardify-trailer__youtube {      
         position: fixed !important;      
-        top: 15% !important;      
+        top: 10% !important;      
         right: 2em !important;      
         bottom: auto !important;      
         left: auto !important;      
