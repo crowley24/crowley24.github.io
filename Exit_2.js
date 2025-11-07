ee20a58d806c68d693e4c7dbb43a181dac18a343
crowley24 <unclezhenya26@gmail.com>
2025-11-07 19:22:06 +0200

    Update Exit_2.js

--- Exit_2.js
+++ Exit_2.js
@@ -1,60 +1,131 @@
-(function () {
-    "use strict";
-
-    Lampa.Lang.add({
-        exit_menu: {
-            ru: "Выход",
-            en: "Exit",
-            uk: "Вихід",
-            be: "Вынахад",
-            zh: "出口",
-            pt: "Saída",
-            bg: "Изход"
-        }
-    });
-
-    function exit_m(object) {
-        this.create = function () { };
-        this.build = function () { }; // this.activity.loader(false);
-        this.start = function () { };
-        this.pause = function () { };
-        this.stop = function () { };
-        this.render = function () { };
-        this.destroy = function () { };
-    }
-    function add() {
-        var ico =
-            '<svg version="1.1" id="exit" color="#fff" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"\n   viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve">\n<g>\n  <path fill="currentColor" d="M256,5.1c138.6,0,250.9,112.3,250.9,250.9S394.6,506.9,256,506.9S5.1,394.6,5.1,256S117.4,5.1,256,5.1z\n     M256,40.1C136.7,40.1,40.1,136.7,40.1,256S136.7,471.9,256,471.9S471.9,375.3,471.9,256S375.3,40.1,256,40.1z M311.4,176.6\n    c6.7-6.7,17.5-6.7,24.2,0c6.7,6.7,6.7,17.5,0,24.2l-55.1,55.1l55.1,55c6.7,6.7,6.7,17.5,0,24.2c-6.7,6.7-17.5,6.7-24.2,0L256.3,280\n    l-55.1,55.1c-6,6-15.4,6.6-22.1,1.8l-2.2-1.8c-6.7-6.7-6.7-17.5,0-24.2l55.1-55l-55.1-55c-6.7-6.7-6.7-17.5,0-24.2s17.5-6.7,24.2,0\n    l55.1,55.1L311.4,176.6z"/>\n</g>\n</svg>';
-        var menu_items = $(
-            '<li class="menu__item selector" data-action="exit_r"><div class="menu__ico">' +
-            ico +
-            '</div><div class="menu__text">' +
-            Lampa.Lang.translate("exit_menu") +
-            "</div></li>"
-        );
-        menu_items.on("hover:enter", function () {
-            Lampa.Activity.out();
-            if (Lampa.Platform.is('apple_tv'))
-                window.location.assign('exit://exit');
-            if (Lampa.Platform.is("tizen"))
-                tizen.application.getCurrentApplication().exit();
-            if (Lampa.Platform.is("webos")) window.close();
-            if (Lampa.Platform.is("android")) Lampa.Android.exit();
-            if (Lampa.Platform.is("orsay")) Lampa.Orsay.exit();
-            if (Lampa.Platform.is("nw")) nw.Window.get().close();
-        });
-        $(".menu .menu__list").eq(1).append(menu_items);
-    }
-
-    function createExitMenu() {
-        window.plugin_exit_m_ready = true;
-        Lampa.Component.add("exit_m", exit_m);
-        if (window.appready) add();
-        else {
-            Lampa.Listener.follow("app", function (e) {
-                if (e.type == "ready") add();
-            });
-        }
-    }
-    if (!window.plugin_exit_m_ready) createExitMenu();
+(function() {  
+  'use strict';  
+    
+  function waitForLampa(callback) {  
+    if (window.Lampa && Lampa.Storage && Lampa.Lang && Lampa.Activity) {  
+      callback();  
+    } else {  
+      setTimeout(function() { waitForLampa(callback); }, 100);  
+    }  
+  }  
+    
+  waitForLampa(function() {  
+    if (window.logout_button_plugin) return;  
+    window.logout_button_plugin = true;  
+      
+    console.log('[LogoutButton] Ініціалізація плагіна');  
+      
+    // Локалізація  
+    Lampa.Lang.add({  
+      logout_button: {  
+        en: 'Exit',  
+        uk: 'Вихід',  
+        ru: 'Выход'  
+      },  
+      logout_confirm: {  
+        en: 'Are you sure you want to exit?',  
+        uk: 'Ви впевнені, що хочете вийти?',  
+        ru: 'Вы уверены, что хотите выйти?'  
+      }  
+    });  
+      
+    // Створення кнопки виходу  
+    function createLogoutButton() {  
+      var button = document.createElement('div');  
+      button.className = 'head__action logout-button selector';  
+      button.innerHTML = `  
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">  
+          <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>  
+          <polyline points="16 17 21 12 16 7" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>  
+          <line x1="21" y1="12" x2="9" y2="12" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>  
+        </svg>  
+        <span style="margin-left: 0.5em;">${Lampa.Lang.translate('logout_button')}</span>  
+      `;  
+        
+      button.style.cssText = `  
+        display: inline-flex;  
+        align-items: center;  
+        padding: 0.5em 1em;  
+        margin-left: 1em;  
+        cursor: pointer;  
+        border-radius: 8px;  
+        transition: background 0.2s;  
+      `;  
+        
+      // Hover ефект  
+      button.addEventListener('mouseenter', function() {  
+        button.style.background = 'rgba(255, 255, 255, 0.1)';  
+      });  
+        
+      button.addEventListener('mouseleave', function() {  
+        button.style.background = 'transparent';  
+      });  
+        
+      // Обробник кліку  
+      button.addEventListener('click', function() {  
+        Lampa.Select.show({  
+          title: Lampa.Lang.translate('logout_button'),  
+          items: [  
+            {  
+              title: Lampa.Lang.translate('logout_confirm'),  
+              yes: true  
+            },  
+            {  
+              title: Lampa.Lang.translate('cancel'),  
+              cancel: true  
+            }  
+          ],  
+          onSelect: function(item) {  
+            if (item.yes) {  
+              // Очистити дані користувача  
+              if (Lampa.Account && typeof Lampa.Account.logout === 'function') {  
+                Lampa.Account.logout();  
+              }  
+                
+              // Очистити Storage  
+              Lampa.Storage.set('account', {});  
+              Lampa.Storage.set('account_user', null);  
+                
+              // Перезавантажити сторінку  
+              window.location.reload();  
+            }  
+              
+            Lampa.Controller.toggle('content');  
+          },  
+          onBack: function() {  
+            Lampa.Controller.toggle('content');  
+          }  
+        });  
+      });  
+        
+      return button;  
+    }  
+      
+    // Додавання кнопки в header  
+    function insertButton() {  
+      var header = document.querySelector('.head');  
+      if (!header) {  
+        setTimeout(insertButton, 100);  
+        return;  
+      }  
+        
+      var actions = header.querySelector('.head__actions');  
+      if (actions) {  
+        var button = createLogoutButton();  
+        actions.appendChild(button);  
+        console.log('[LogoutButton] Кнопку додано в header');  
+      }  
+    }  
+      
+    // Ініціалізація  
+    if (Lampa.Listener) {  
+      Lampa.Listener.follow('app', function(e) {  
+        if (e.type === 'ready') {  
+          insertButton();  
+        }  
+      });  
+    } else {  
+      setTimeout(insertButton, 1000);  
+    }  
+  });  
 })();
