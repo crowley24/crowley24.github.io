7c2a4ece97fb56a0500996fcc7ff438d7c0b87f8
crowley24 <unclezhenya26@gmail.com>
2025-12-04 01:11:10 +0200

    Update quality_test2.js

--- quality_test2.js
+++ quality_test2.js
@@ -20,7 +20,7 @@
         /* Контейнер бейджів */  
         .card__quality-badge {  
             position: absolute !important;  
-            top: 8px !important;  
+            top: 7px !important;  
             left: 7px !important;  
             display: flex !important;  
             gap: 4px !important;  
