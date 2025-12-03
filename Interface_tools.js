53f5af0cbae7b06b7dee832ad978739149490a25
crowley24 <unclezhenya26@gmail.com>
2025-12-03 13:04:32 +0200

    Update Interface_tools.js

--- Interface_tools.js
+++ Interface_tools.js
@@ -32,12 +32,10 @@
         if (Lampa.Settings.main && !Lampa.Settings.main().render().find('[data-component="foxstudio"]').length) {  
             var field = "<div class=\"settings-folder selector\" data-component=\"foxstudio\" data-static=\"true\">\n\t\t\t<div class=\"settings-folder__icon\">\n\t\t\t\t<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width=\"512\" height=\"512\" viewBox=\"0 0 490 490\" xml:space=\"preserve\"><path d=\"M153.125 317.435h183.75v30.625h-183.75z\" fill=\"white\"></path><circle cx=\"339.672\" cy=\"175.293\" r=\"42.642\" fill=\"white\"></circle><path d=\"M420.914 0H69.086C30.999 0 0 30.999 0 69.086v351.829C0 459.001 30.999 490 69.086 490h351.829C459.001 490 490 459.001 490 420.914V69.086C490 30.999 459.001 0 420.914 0zM69.086 30.625h237.883c-17.146 20.912-42.277 47.893-75.177 74.575-9.514-12.906-26.35-19.331-42.586-14.613l-69.644 20.242c-20.778 6.039-32.837 27.98-26.798 48.758l6.475 22.278c-21.375 8-44.353 14.456-68.614 19.267V69.086c0-21.204 17.257-38.461 38.461-38.461zm390.289 390.289c0 21.204-17.257 38.461-38.461 38.461H69.086c-21.204 0-38.461-17.257-38.461-38.461V232.459c27.504-4.993 53.269-12.075 77.268-20.816l3.811 13.111c6.038 20.778 27.98 32.837 48.758 26.799l69.643-20.242c20.778-6.039 32.837-27.98 26.799-48.758l-13.481-46.382c50.532-39.47 84.67-80.759 102.687-105.546h74.805c21.204 0 38.461 17.257 38.461 38.461v351.828z\" fill=\"white\"></path></svg>\n\t\t\t</div>\n\t\t\t<div class=\"settings-folder__name\">"+Lampa.Lang.translate('foxstudio_title')+"</div>\n\t\t</div>";  
               
-            // Додаємо після розділу "more" або в кінець  
             var moreElement = Lampa.Settings.main().render().find('[data-component="more"]');  
             if (moreElement.length) {  
                 moreElement.after(field);  
             } else {  
-                // Якщо "more" не знайдено, додаємо в кінець  
                 Lampa.Settings.main().render().append(field);  
             }  
               
@@ -47,8 +45,8 @@
   
     // Функція показу налаштувань FoxStudio (вирішення проблеми з порожнім екраном)  
     function showFoxStudioSettings() {  
-        // Створюємо контейнер для налаштувань  
-        var settingsBody = $('<div class="settings-body"></div>');  
+        // Створюємо контент для налаштувань  
+        var settingsContent = $('<div class="settings__body"></div>');  
           
         // Основна настройка інтерфейсу  
         var foxstudio_interface = $('<div class="settings-param selector" data-type="toggle" data-name="foxstudio_interface_enabled">');  
@@ -66,9 +64,9 @@
         logo_setting.append('<div class="settings-param__value">' + (Lampa.Storage.get('logo_enabled', false) ? 'Вкл' : 'Выкл') + '</div>');  
   
         // Додаємо елементи в контейнер  
-        settingsBody.append(foxstudio_interface);  
-        settingsBody.append(necardify_setting);  
-        settingsBody.append(logo_setting);  
+        settingsContent.append(foxstudio_interface);  
+        settingsContent.append(necardify_setting);  
+        settingsContent.append(logo_setting);  
   
         // Обробники зміни налаштувань  
         foxstudio_interface.on('hover:enter', function() {  
@@ -100,19 +98,16 @@
             }  
         });  
   
-        // Створюємо екран налаштувань  
+        // Створюємо екран налаштувань з нашим контентом  
         Lampa.Activity.push({  
             url: '',  
             title: Lampa.Lang.translate('foxstudio_title'),  
             component: 'settings',  
             page: 1,  
-            nocache: true  
+            nocache: true,  
+            template: 'settings',  
+            content: settingsContent  
         });  
-  
-        // Додаємо наші налаштування до створеного екрана  
-        setTimeout(function() {  
-            $('.settings-body').append(settingsBody);  
-        }, 100);  
     }  
   
     // Функція ініціалізації плагіна  
@@ -144,7 +139,6 @@
         // Додаємо обробник для головного екрана налаштувань  
         Lampa.Settings.listener.follow('open', function(e) {  
             if (e.name === 'main') {  
-                // Прикріплюємо обробник кліку до нашого елемента  
                 e.body.find('[data-component="foxstudio"]').on('hover:enter', function() {  
                     showFoxStudioSettings();  
                 });  
