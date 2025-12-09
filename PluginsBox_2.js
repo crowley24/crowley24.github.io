718924b81a32eaabcdf169afc86dc4809a5ad817
crowley24 <unclezhenya26@gmail.com>
2025-12-09 13:20:19 +0200

    Update PluginsBox_2.js

--- PluginsBox_2.js
+++ PluginsBox_2.js
@@ -383,63 +383,63 @@ Lampa.SettingsApi.addParam({
     }  
 });
 
-/* Tricks */
-Lampa.SettingsApi.addParam({
-    component: 'add_interface_plugin',
-    param: {
-        name: 'Tricks',
-        type: 'select',
-        values: {
-            1: 'Встановити',
-            2: 'Видалити'
-        },
-    },
-    field: {
-        name: 'Приємні дрібниці',
-        description: 'Плагін дозволяє обрати доповнення (скрінсейвери, стиль кнопок, стиль плеєра тощо)'
-    },
-    onChange: function(value) {
-        if (value == '1') {
-            itemON('https://andreyurl54.github.io/diesel5/tricks.js', 'Приємні дрібниці', '@AndreyURL54', 'Tricks');
-        }
-        if (value == '2') {
-            deletePlugin("https://andreyurl54.github.io/diesel5/tricks.js");
-        }
-    },
-    onRender: function (item) {
-        $('.settings-param__name', item).css('color','#f3d900');
-        hideInstall();
-
-        var myResult = checkPlugin('https://andreyurl54.github.io/diesel5/tricks.js');
-        var pluginsArray = Lampa.Storage.get('plugins') || [];
-
-        setTimeout(function() {
-            var container = $('div[data-name="Tricks"]');
-            if (container.length && container.find('.settings-param__status').length === 0) {
-                container.append('<div class="settings-param__status one"></div>');
-            }
-            var pluginStatus = null;
-            for (var i = 0; i < pluginsArray.length; i++) {
-                if (pluginsArray[i] && pluginsArray[i].url === 'https://andreyurl54.github.io/diesel5/tricks.js') {
-                    pluginStatus = pluginsArray[i].status;
-                    break;
-                }
-            }
-            var statusEl = $('div[data-name="Tricks"]').find('.settings-param__status');
-            statusEl.removeClass('active error').css('background-color','');
-            if (myResult && pluginStatus !== 0) {
-                statusEl.addClass('active');
-            } else if (pluginStatus === 0) {
-                statusEl.css('background-color', 'rgb(255, 165, 0)');
-            } else {
-                statusEl.addClass('error');
-            }
-        }, 100);
-
-        item.on("hover:enter", function (event) {
-            nthChildIndex = focus_back(event);
-        });
-    }
+/* MobileLogo */  
+Lampa.SettingsApi.addParam({  
+    component: 'add_interface_plugin',  
+    param: {  
+        name: 'MOBILELOGO',  
+        type: 'select',  
+        values: {  
+            1: 'Встановити',  
+            2: 'Видалити'  
+        },  
+    },  
+    field: {  
+        name: 'MobileLogo',  
+        description: 'Мобільні логотипи для інтерфейсу'  
+    },  
+    onChange: function(value) {  
+        if (value == '1') {  
+            itemON('https://crowley24.github.io/logo+mob.js', 'MobileLogo', '@lampa', 'MOBILELOGO');  
+        }  
+        if (value == '2') {  
+            deletePlugin("https://crowley24.github.io/logo+mob.js");  
+        }  
+    },  
+    onRender: function (item) {  
+        $('.settings-param__name', item).css('color','#f3d900');  
+        hideInstall();  
+  
+        var myResult = checkPlugin('https://crowley24.github.io/logo+mob.js');  
+        var pluginsArray = Lampa.Storage.get('plugins') || [];  
+  
+        setTimeout(function() {  
+            var container = $('div[data-name="MOBILELOGO"]');  
+            if (container.length && container.find('.settings-param__status').length === 0) {  
+                container.append('<div class="settings-param__status one"></div>');  
+            }  
+            var pluginStatus = null;  
+            for (var i = 0; i < pluginsArray.length; i++) {  
+                if (pluginsArray[i] && pluginsArray[i].url === 'https://crowley24.github.io/logo+mob.js') {  
+                    pluginStatus = pluginsArray[i].status;  
+                    break;  
+                }  
+            }  
+            var statusEl = $('div[data-name="MOBILELOGO"]').find('.settings-param__status');  
+            statusEl.removeClass('active error').css('background-color','');  
+            if (myResult && pluginStatus !== 0) {  
+                statusEl.addClass('active');  
+            } else if (pluginStatus === 0) {  
+                statusEl.css('background-color', 'rgb(255, 165, 0)');  
+            } else {  
+                statusEl.addClass('error');  
+            }  
+        }, 100);  
+  
+        item.on("hover:enter", function (event) {  
+            nthChildIndex = focus_back(event);  
+        });  
+    }  
 });
 
 /* Прослуховування при відкритті сторінки "add_plugin" для очищення артефактів */
