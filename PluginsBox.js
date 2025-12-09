d22ce6ef55447b4078db48a8ed6e0cc084d70f5d
crowley24 <unclezhenya26@gmail.com>
2025-12-09 22:45:33 +0200

    Update PluginsBox.js

--- PluginsBox.js
+++ PluginsBox.js
@@ -463,6 +463,68 @@ function addonStart() {
         }  
     });  
 
+    
+    // Themes - Додано напряму в add_plugin  
+    Lampa.SettingsApi.addParam({  
+        component: 'add_plugin',  
+        param: {  
+            name: 'THEMES',  
+            type: 'select',  
+            values: {  
+                1: 'Встановити',  
+                2: 'Видалити'  
+            },  
+        },  
+        field: {  
+            name: 'Теми для інтерфейса',  
+            description: 'Теми для інтерфейсу'  
+        },  
+        onChange: function(value) {  
+            if (value == '1') {  
+                itemON('https://crowley24.github.io/themes.js', 'Themes', '@lampa', 'THEMES');  
+            }  
+            if (value == '2') {  
+                deletePlugin("https://crowley24.github.io/themes.js");  
+            }  
+        },  
+        onRender: function(item) {  
+            $('.settings-param__name', item).css('color', '#f3d900');  
+            hideInstall();  
+              
+            var myResult = checkPlugin('https://crowley24.github.io/themes.js');  
+            var pluginsArray = Lampa.Storage.get('plugins') || [];  
+              
+            setTimeout(function() {  
+                var container = $('div[data-name="THEMES"]');  
+                if (container && container.length && container.find('.settings-param__status').length === 0) {  
+                    container.append('<div class="settings-param__status one"></div>');  
+                }  
+                var pluginStatus = null;  
+                for (var i = 0; i < pluginsArray.length; i++) {  
+                    if (pluginsArray[i] && pluginsArray[i].url === 'https://crowley24.github.io/themes.js') {  
+                        pluginStatus = pluginsArray[i].status;  
+                        break;  
+                    }  
+                }  
+                var statusEl = $('div[data-name="THEMES"]').find('.settings-param__status');  
+                if (statusEl && statusEl.length) {  
+                    statusEl.removeClass('active error').css('background-color', '');  
+                    if (myResult && pluginStatus !== 0) {  
+                        statusEl.addClass('active');  
+                    } else if (pluginStatus === 0) {  
+                        statusEl.css('background-color', 'rgb(255, 165, 0)');  
+                    } else {  
+                        statusEl.addClass('error');  
+                    }  
+                }  
+            }, 100);  
+              
+            item.on("hover:enter", function(event) {  
+                nthChildIndex = focus_back(event);  
+            });  
+        }  
+    });  
+
     // Interface_hide - Додано напряму в add_plugin  
     Lampa.SettingsApi.addParam({  
         component: 'add_plugin',  
