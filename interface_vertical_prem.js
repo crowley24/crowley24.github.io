b0e19b6be03c57a815b703dd798522171e178b7a
crowley24 <unclezhenya26@gmail.com>
2025-12-29 00:02:41 +0200

    Update interface_vertical_prem.js

--- interface_vertical_prem.js
+++ interface_vertical_prem.js
@@ -857,7 +857,7 @@ card.use({
     --ni-info-h: clamp(15em, 34vh, 24em);
 }
 html:not(.is-smarttv) .new-interface{
-    --ni-card-w: clamp(120px, 7.6vw, 170px);
+    --ni-card-w: clamp(100px, 6.5vw, 140px); /* Зменшені значення */
 }
 
 /* Размер карточек и плитки "Ещё" */
