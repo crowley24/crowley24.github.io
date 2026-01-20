72ac1d976d5ab407eaf1c9241f97bedee6ce9fa2
crowley24 <unclezhenya26@gmail.com>
2026-01-20 22:26:21 +0200

    Update Random_2.js

--- Random_2.js
+++ Random_2.js
@@ -56,7 +56,7 @@
     });  
   
     Lampa.SettingsApi.addParam({  
-      component: 'plugins',   
+      component: 'plugins',  
       param: {  
         name: 'lampa_random_vote_to',  
         type: 'select',  
@@ -147,7 +147,7 @@
   function setVoteRange(f,t){f=roundHalf(clamp(f,1,10)); t=roundHalf(clamp(t,1,10)); if(f>t)t=f; try{Lampa.Storage.set(STORAGE_VOTE_FROM,f);}catch(e){} try{Lampa.Storage.set(STORAGE_VOTE_TO,t);}catch(e){}}  
   
   function getGenres(){try{return Lampa.Storage.get(STORAGE_GENRES,[]);}catch(e){return [];}}  
-  function setGenres(arr){try{Lampa.Storage.set(STORAGE_GENRES,arr);}catch(e){}  
+  function setGenres(arr){try{Lampa.Storage.set(STORAGE_GENRES,arr);}catch(e){}}  // ВИПРАВЛЕНО: додано закриваючу дужку  
   
   function getYearFrom(){return Lampa.Storage.get(STORAGE_YEAR_FROM,1980);}  
   function getYearTo(){return Lampa.Storage.get(STORAGE_YEAR_TO, nowYear());}  
