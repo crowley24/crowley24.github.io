fbf148a6ffec82829a6e0e9d1ba81d269cd8ee3b
crowley24 <unclezhenya26@gmail.com>
2026-02-17 15:38:47 +0200

    Update Wert.js

--- Wert.js
+++ Wert.js
@@ -1,7 +1,7 @@
 (function() {
     'use strict';
 
-    // Створюємо налаштування в сховищі за замовчуванням
+    // Ініціалізація налаштування
     if (Lampa.Storage.get('mobile_interface_animation', 'unset') === 'unset') {
         Lampa.Storage.set('mobile_interface_animation', true);
     }
@@ -23,31 +23,10 @@
             @media screen and (max-width: 480px) {
                 .background { background: #000 !important; }
 
-                .full-start__poster,
-                .full-start-new__poster,
-                .full-start__poster img,
-                .full-start-new__poster img,
-                .screensaver__slides-slide img,
-                .screensaver__bg {
+                .full-start__poster, .full-start-new__poster, 
+                .full-start__poster img, .full-start-new__poster img {
                     filter: none !important;
                     -webkit-filter: none !important;
-                }
-                
-                .full-start-new__right {
-                    background: none !important;
-                    border: none !important;
-                    box-shadow: none !important;
-                    z-index: 2 !important;
-                    margin-top: -120px !important;
-                    display: flex !important;
-                    flex-direction: column !important;
-                    align-items: center !important;
-                }
-
-                .full-start-new__right::before, 
-                .full-start-new__right::after {
-                    display: none !important;
-                    content: unset !important;
                 }
                 
                 .full-start-new__poster {
@@ -55,46 +34,33 @@
                     overflow: visible !important;
                 }
                 
-                .full-start-new__poster img, .full--poster {
-                    /* ПЕРЕВІРКА ТРИГЕРА АНІМАЦІЇ */
+                .full-start-new__poster img {
                     ${isAnimationEnabled ? 'animation: kenBurnsEffect 30s ease-in-out infinite !important;' : 'animation: none !important;'}
                     transform-origin: center center !important;
-                    
-                    mask-image: linear-gradient(to bottom, 
-                        rgba(0, 0, 0, 1) 0%,
-                        rgba(0, 0, 0, 1) 50%,
-                        rgba(0, 0, 0, 0.8) 70%,
-                        rgba(0, 0, 0, 0.4) 85%,
-                        rgba(0, 0, 0, 0) 100%) !important;
-                    -webkit-mask-image: linear-gradient(to bottom, 
-                        rgba(0, 0, 0, 1) 0%,
-                        rgba(0, 0, 0, 1) 50%,
-                        rgba(0, 0, 0, 0.8) 70%,
-                        rgba(0, 0, 0, 0.4) 85%,
-                        rgba(0, 0, 0, 0) 100%) !important;
+                    mask-image: linear-gradient(to bottom, rgba(0,0,0,1) 0%, rgba(0,0,0,1) 50%, rgba(0,0,0,0.8) 70%, rgba(0,0,0,0.4) 85%, rgba(0,0,0,0) 100%) !important;
+                    -webkit-mask-image: linear-gradient(to bottom, rgba(0,0,0,1) 0%, rgba(0,0,0,1) 50%, rgba(0,0,0,0.8) 70%, rgba(0,0,0,0.4) 85%, rgba(0,0,0,0) 100%) !important;
                 }
                 
                 .full-start-new__img {
                     border-radius: 0 !important;
-                    mask-image: linear-gradient(to bottom, 
-                        rgba(0, 0, 0, 0) 0%,
-                        rgba(0, 0, 0, 1) 30%,
-                        rgba(0, 0, 0, 1) 70%,
-                        rgba(0, 0, 0, 0) 100%) !important;
-                    -webkit-mask-image: linear-gradient(to bottom, 
-                        rgba(0, 0, 0, 0) 0%,
-                        rgba(0, 0, 0, 1) 30%,
-                        rgba(0, 0, 0, 1) 70%,
-                        rgba(0, 0, 0, 0) 100%) !important;
+                    mask-image: linear-gradient(to bottom, rgba(0,0,0,0) 0%, rgba(0,0,0,1) 30%, rgba(0,0,0,1) 70%, rgba(0,0,0,0) 100%) !important;
+                    -webkit-mask-image: linear-gradient(to bottom, rgba(0,0,0,0) 0%, rgba(0,0,0,1) 30%, rgba(0,0,0,1) 70%, rgba(0,0,0,0) 100%) !important;
                 }
                 
-                .full-start-new__title {
-                    width: 100% !important;
+                .full-start-new__right {
+                    background: none !important;
+                    border: none !important;
+                    box-shadow: none !important;
+                    margin-top: -120px !important;
+                    z-index: 2 !important;
                     display: flex !important;
-                    justify-content: center !important;
-                    min-height: 70px !important;
+                    flex-direction: column !important;
+                    align-items: center !important;
                 }
-
+                
+                .full-start-new__right::before, .full-start-new__right::after { content: unset !important; }
+                
+                .full-start-new__title { width: 100%; display: flex; justify-content: center; min-height: 70px; }
                 .full-start-new__buttons, .full-start-new__details, .full-descr__text, .full-start-new__tagline {
                     justify-content: center !important;
                     text-align: center !important;
@@ -105,16 +71,17 @@
         document.head.appendChild(style);
     }
 
-    // Додаємо пункт меню в налаштування
-    function addSettingsMenu() {
+    // Додавання меню налаштувань
+    function addSettings() {
+        // Слухаємо подію створення налаштувань
         Lampa.Settings.listener.follow('open', function (e) {
             if (e.name === 'main') {
-                var folder = $('<div class="settings-folder selector" data-name="mobile_interface">' +
+                var item = $('<div class="settings-folder selector" data-name="mobile_interface">' +
                     '<div class="settings-folder__icon"><svg height="36" viewBox="0 0 24 24" width="36" xmlns="http://www.w3.org/2000/svg"><path d="M17 1.01L7 1c-1.1 0-2 .9-2 2v18c0 1.1.9 2 2 2h10c1.1 0 2-.9 2-2V3c0-1.1-.9-1.99-2-1.99zM17 19H7V5h10v14z" fill="white"/></svg></div>' +
                     '<div class="settings-folder__name">Мобільний інтерфейс</div>' +
                 '</div>');
 
-                folder.on('hover:enter', function () {
+                item.on('hover:enter', function () {
                     Lampa.Settings.create({
                         title: 'Мобільний інтерфейс',
                         items: [
@@ -130,38 +97,40 @@
                             }
                         ],
                         onBack: function() {
-                            applyStyles(); // Оновлюємо анімацію при виході
+                            applyStyles(); // Оновлюємо стилі при поверненні
                             Lampa.Settings.main();
                         }
                     });
                 });
-                $('.settings-main').append(folder);
+
+                // Додаємо пункт меню перед розділом "Інше" або в кінець
+                var other = $('.settings-main .selector[data-name="more"]');
+                if (other.length) other.before(item);
+                else $('.settings-main').append(item);
             }
         });
     }
 
-    // Логіка Логотипів
-    function initLogoPlugin() {
+    function initLogo() {
         Lampa.Listener.follow('full', function(e) {
             if (window.innerWidth <= 480 && e.type === 'complite') {
-                var data = e.data.movie;
-                var type = data.name ? 'tv' : 'movie';
+                var movie = e.data.movie;
+                var type = movie.name ? 'tv' : 'movie';
                 var lang = Lampa.Storage.get('language');
                 
-                var url = Lampa.TMDB.api(type + '/' + data.id + '/images?api_key=' + Lampa.TMDB.key() + '&language=' + lang);
-                
+                var url = Lampa.TMDB.api(type + '/' + movie.id + '/images?api_key=' + Lampa.TMDB.key() + '&language=' + lang);
                 $.get(url, function(res) {
                     let path = (res.logos && res.logos[0]) ? res.logos[0].file_path : null;
                     if (!path) {
-                        $.get(Lampa.TMDB.api(type + '/' + data.id + '/images?api_key=' + Lampa.TMDB.key() + '&language=en'), function(resEn) {
+                        $.get(Lampa.TMDB.api(type + '/' + movie.id + '/images?api_key=' + Lampa.TMDB.key() + '&language=en'), function(resEn) {
                             if (resEn.logos && resEn.logos[0]) render(resEn.logos[0].file_path);
                         });
                     } else render(path);
                 });
 
                 function render(p) {
-                    const img = Lampa.TMDB.image('/t/p/w300' + p.replace('.svg', '.png'));
-                    e.object.activity.render().find('.full-start-new__title').html('<img src="'+img+'" style="max-height: 120px; object-fit: contain; position: relative; z-index: 10;">');
+                    const imgUrl = Lampa.TMDB.image('/t/p/w300' + p.replace('.svg', '.png'));
+                    e.object.activity.render().find('.full-start-new__title').html('<img src="'+imgUrl+'" style="max-height: 120px; object-fit: contain; position: relative; z-index: 10;">');
                 }
             }
         });
@@ -169,9 +138,10 @@
 
     function start() {
         applyStyles();
-        addSettingsMenu();
-        initLogoPlugin();
+        addSettings();
+        initLogo();
         
+        // Вимикаємо стандартний блюр Lampa
         setInterval(function() {
             if (window.innerWidth <= 480 && window.lampa_settings) {
                 window.lampa_settings.blur_poster = false;
