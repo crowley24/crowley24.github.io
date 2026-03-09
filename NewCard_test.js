0bb07c3c4c22bd4f4a7a25e7391112c49e2b8147
crowley24 <unclezhenya26@gmail.com>
2026-03-09 19:39:27 +0200

    Update NewCard_test.js

--- NewCard_test.js
+++ NewCard_test.js
@@ -1,7 +1,6 @@
 (function () {  
     'use strict';  
-  
-    const PLUGIN_NAME = 'NewCard';  
+      const PLUGIN_NAME = 'NewCard';  
     const PLUGIN_ID = 'new_card_style';  
     const ASSETS_PATH = 'https://crowley38.github.io/Icons/';  
     const CACHE_LIFETIME = 1000 * 60 * 60 * 24; // 24 години  
