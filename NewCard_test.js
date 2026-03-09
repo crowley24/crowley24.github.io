787ebfb74a44b4dc682949c85019ef7fc64d9015
crowley24 <unclezhenya26@gmail.com>
2026-03-09 09:21:41 +0200

    Update NewCard_test.js

--- NewCard_test.js
+++ NewCard_test.js
@@ -216,7 +216,7 @@
 .cas-rate-item img { height: 1.1em; }
 
 .left-title .full-start-new__body { height: 85vh; }
-.left-title .full-start-new__right { display: flex; align-items: flex-end; padding-bottom: 2vh; padding-left: 1.5%; }
+.left-title .full-start-new__right { display: flex; align-items: flex-end; padding-bottom: 2vh; padding-left: 1%; }
 .left-title .full-start-new__reactions, .left-title .full-start-new__rate-line, .left-title .full-start__status, .left-title .rating--modss, .left-title .full-start-new__head, .left-title .full-start-new__details { display: none !important; }
 
 @keyframes casKenBurns { 0% { transform: scale(1); } 50% { transform: scale(1.1); } 100% { transform: scale(1); } }
