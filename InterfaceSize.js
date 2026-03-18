8dcd571a581e447d7726c2b57b3d85a425aab43e
crowley24 <unclezhenya26@gmail.com>
2026-03-18 20:57:58 +0200

    Update InterfaceSize.js

--- InterfaceSize.js
+++ InterfaceSize.js
@@ -10,13 +10,13 @@
   Lampa.Manifest.plugins = manifest;  
   
   const lang_data = {
-    settings_param_interface_size_mini: 'Міні',  
-    settings_param_interface_size_very_small: 'Дуже малий',  
-    settings_param_interface_size_small: 'Малий',  
-    settings_param_interface_size_medium: 'Середній',  
-    settings_param_interface_size_standard: 'Стандартний',  
-    settings_param_interface_size_large: 'Великий',  
-    settings_param_interface_size_very_large: 'Дуже великий'
+    settings_param_interface_size_mini: 'Міні інтерфейс',  
+    settings_param_interface_size_very_small: 'Дуже малий інтерфейс',  
+    settings_param_interface_size_small: 'Малий інтерфейс',  
+    settings_param_interface_size_medium: 'Середній інтерфейс',  
+    settings_param_interface_size_standard: 'Стандартний інтерфейс',  
+    settings_param_interface_size_large: 'Великий інтерфейс',  
+    settings_param_interface_size_very_large: 'Дуже великий інтерфейс'
   };
 
   function init() {
@@ -24,31 +24,29 @@
       Lampa.Lang.add(lang_data);
     }
 
-    // Очищуємо старі значення, щоб прибрати "Менше" та вирівняти порядок
     Lampa.Params.values['interface_size'] = {};
 
-    // Додаємо значення в суворому порядку від 9 до 12
     Lampa.Params.select('interface_size', {  
-      '09': lang_data.settings_param_interface_size_mini,        
-      '09.5': lang_data.settings_param_interface_size_very_small, 
-      '10': lang_data.settings_param_interface_size_small,       
-      '10.5': lang_data.settings_param_interface_size_medium,    
-      '11': lang_data.settings_param_interface_size_standard,    
-      '11.5': lang_data.settings_param_interface_size_large,     
-      '12': lang_data.settings_param_interface_size_very_large   
-    }, '11');  
+      '09.1': lang_data.settings_param_interface_size_mini,        
+      '09.6': lang_data.settings_param_interface_size_very_small, 
+      '10.1': lang_data.settings_param_interface_size_small,       
+      '10.6': lang_data.settings_param_interface_size_medium,    
+      '11.1': lang_data.settings_param_interface_size_standard,    
+      '11.6': lang_data.settings_param_interface_size_large,     
+      '12.1': lang_data.settings_param_interface_size_very_large   
+    }, '11.1');  
 
     updateSize();
   }
   
   const updateSize = () => {
-    const iSize = Lampa.Platform.screen('mobile') ? 10 : parseFloat(Lampa.Storage.field('interface_size')) || 11;
+    const iSize = Lampa.Platform.screen('mobile') ? 10.1 : parseFloat(Lampa.Storage.field('interface_size')) || 11.1;
     $('body').css({ fontSize: iSize + 'px' });  
   
     // Логіка карток
     let cardCount = 6;
-    if (iSize <= 9.5) cardCount = 8;
-    else if (iSize <= 11) cardCount = 7;
+    if (iSize <= 9.6) cardCount = 8;
+    else if (iSize <= 11.1) cardCount = 7;
 
     if (Lampa.Maker && Lampa.Maker.map) {
       ['Line', 'Category'].forEach(type => {
