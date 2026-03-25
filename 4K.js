0dc12197c154dcc1286c8b68410ac03580422637
crowley24 <unclezhenya26@gmail.com>
2026-03-25 11:46:17 +0200

    Update 4K.js

--- 4K.js
+++ 4K.js
@@ -1,158 +1,164 @@
 (function () {
     'use strict';
 
-    const PLUGIN_NAME = 'UaDV Prime';
-    const VERSION = '1.0.0';
+    // Функція сповіщень
+    function noty(msg) {
+        if (window.Lampa && Lampa.Noty) {
+            Lampa.Noty.show(msg);
+        } else {
+            console.log('UaDV Log:', msg);
+        }
+    }
 
-    // Конфігурація
-    const CONFIG = {
-        min_seeders: 3,
-        max_size: 90 * 1024 * 1024 * 1024, // 90GB
-        timeout: 15000,
-        categories: '2000,2010,2030,2040,2045,5000,5030,5040' // Фільми + Серіали
+    // Основна логіка пошуку та запуску
+    var UaDV = {
+        run: async function (object) {
+            var movie = object.movie;
+            if (!movie) return noty('Помилка: дані фільму не знайдено');
+
+            // Формуємо запит (Назва + Рік)
+            var year = (movie.release_date || movie.first_air_date || '').slice(0, 4);
+            var query = (movie.original_title || movie.original_name || movie.title || movie.name) + (year ? ' ' + year : '');
+            
+            noty('Пошук кращого UA релізу: ' + query);
+
+            try {
+                var url = Lampa.Storage.field('jackett_url');
+                var key = Lampa.Storage.field('jackett_key');
+                
+                if (!url) {
+                    noty('Помилка: Вкажіть Jackett URL у налаштуваннях');
+                    return;
+                }
+
+                // Категорії: 2000 (Фільми), 5000 (Серіали)
+                var cats = '2000,2010,2030,2040,5000,5030,5040';
+                var searchUrl = url.replace(/\/$/, '') + '/api/v2.0/indexers/all/results?apikey=' + key + '&Query=' + encodeURIComponent(query) + '&Category[]=' + cats.split(',').join('&Category[]=');
+
+                var response = await fetch(searchUrl);
+                var json = await response.json();
+                var results = json.Results || json.results || [];
+
+                if (!results.length) {
+                    return noty('Нічого не знайдено в Jackett');
+                }
+
+                // Скорринг (бали)
+                var scored = results.map(function(item) {
+                    var score = 0;
+                    var title = (item.Title || '').toLowerCase();
+
+                    // Обов'язково UA
+                    if (title.indexOf('ukr') >= 0 || title.indexOf('ua') >= 0 || title.indexOf('укр') >= 0) {
+                        score += 1000;
+                    } else {
+                        return { item: item, score: -1 };
+                    }
+
+                    // Пріоритет DV/4K
+                    if (title.indexOf('dv') >= 0 || title.indexOf('dolby vision') >= 0) score += 500;
+                    if (title.indexOf('2160p') >= 0 || title.indexOf('4k') >= 0) score += 300;
+                    if (title.indexOf('hdr') >= 0) score += 200;
+                    if (title.indexOf('1080p') >= 0) score += 100;
+                    
+                    // Розмір (якість бітрейту)
+                    score += Math.floor((item.Size || 0) / (1024 * 1024 * 1024)) * 5;
+
+                    return { item: item, score: score };
+                }).filter(function(res) { return res.score > 0; });
+
+                scored.sort(function(a, b) { return b.score - a.score; });
+
+                if (scored.length > 0) {
+                    var best = scored[0].item;
+                    noty('Запуск: ' + best.Title.slice(0, 30));
+                    this.play(best, movie);
+                } else {
+                    noty('Українських релізів не знайдено');
+                }
+
+            } catch (e) {
+                console.error(e);
+                noty('Помилка пошуку');
+            }
+        },
+
+        play: async function (item, movie) {
+            var ts_url = Lampa.Storage.field('torrserver_url').replace(/\/$/, '');
+            var link = item.MagnetUri || item.Link;
+
+            try {
+                var res = await fetch(ts_url + '/torrents', {
+                    method: 'POST',
+                    body: JSON.stringify({
+                        action: 'add',
+                        link: link,
+                        title: '[UaDV] ' + (movie.title || movie.name),
+                        save_to_db: false
+                    })
+                });
+                var data = await res.json();
+                var hash = data.hash;
+
+                var filesRes = await fetch(ts_url + '/torrents', {
+                    method: 'POST',
+                    body: JSON.stringify({ action: 'get', hash: hash })
+                });
+                var filesData = await filesRes.json();
+                var files = filesData.file_stats || [];
+
+                if (files.length) {
+                    // Беремо найбільший файл
+                    var mainFile = files.reduce(function(prev, current) {
+                        return (prev.length > current.length) ? prev : current;
+                    });
+
+                    Lampa.Player.play({
+                        url: ts_url + '/stream/?link=' + hash + '&index=' + mainFile.id + '&play=1',
+                        title: movie.title || movie.name,
+                        timeline: { hash: hash }
+                    });
+                }
+            } catch (e) {
+                noty('Помилка TorrServer');
+            }
+        }
     };
 
-    function noty(msg) {
-        if (window.Lampa && Lampa.Noty) Lampa.Noty.show(msg);
-    }
-
-    // --- Робота з мережею та API ---
-    async function getJackettResults(query) {
-        let url = Lampa.Storage.field('jackett_url');
-        let key = Lampa.Storage.field('jackett_key');
-        
-        if (!url) throw new Error('Вкажіть Jackett URL у налаштуваннях');
-
-        let searchUrl = `${url.replace(/\/$/, '')}/api/v2.0/indexers/all/results?apikey=${key}&Query=${encodeURIComponent(query)}&Category[]=${CONFIG.categories.split(',').join('&Category[]=')}`;
-        
-        try {
-            const response = await fetch(searchUrl);
-            const json = await response.json();
-            return json.Results || json.results || [];
-        } catch (e) {
-            console.error('Jackett error:', e);
-            return [];
-        }
-    }
-
-    // --- Алгоритм вибору найкращого релізу ---
-    function scoreRelease(item) {
-        let score = 0;
-        const title = item.Title.toLowerCase();
-
-        // Пріоритет мови (Українська обов'язкова)
-        if (title.includes('ukr') || title.includes('ua') || title.includes('укр')) score += 1000;
-        else return -1; // Ігноруємо без UA
-
-        // Пріоритет якості відео
-        if (title.includes('dv') || title.includes('dolby vision') || title.includes('dovi')) score += 500;
-        if (title.includes('hdr')) score += 200;
-        if (title.includes('2160p') || title.includes('4k')) score += 300;
-        if (title.includes('1080p')) score += 100;
-
-        // Пріоритет озвучки
-        if (title.includes('dub') || title.includes('дубляж')) score += 150;
-
-        // Вага розміру (якість бітрейту)
-        score += Math.floor(item.Size / (1024 * 1024 * 1024)) * 5; 
-
-        // Сиди (стабільність)
-        score += Math.min(item.Seeders, 50);
-
-        return score;
-    }
-
-    // --- Взаємодія з TorrServer ---
-    async function startPlay(item, movie) {
-        const ts_url = Lampa.Storage.field('torrserver_url').replace(/\/$/, '');
-        const link = item.MagnetUri || item.Link;
-
-        noty('Підготовка потоку...');
-
-        try {
-            // Додаємо торрент без збереження в базу для швидкості
-            const res = await fetch(`${ts_url}/torrents`, {
-                method: 'POST',
-                body: JSON.stringify({
-                    action: 'add',
-                    link: link,
-                    title: `[UaDV] ${movie.title || movie.name}`,
-                    save_to_db: false
-                })
-            });
-            const data = await res.json();
-            const hash = data.hash;
-
-            // Отримуємо список файлів, щоб знайти найбільший (відео)
-            const filesRes = await fetch(`${ts_url}/torrents`, {
-                method: 'POST',
-                body: JSON.stringify({ action: 'get', hash: hash })
-            });
-            const filesData = await filesRes.json();
-            
-            // Шукаємо найбільший файл (зазвичай це фільм)
-            let mainFile = filesData.file_stats.reduce((prev, current) => (prev.length > current.length) ? prev : current);
-
-            const playUrl = `${ts_url}/stream/?link=${hash}&index=${mainFile.id}&play=1`;
-
-            Lampa.Player.play({
-                url: playUrl,
-                title: movie.title || movie.name,
-                timeline: { hash: hash }
-            });
-        } catch (e) {
-            noty('Помилка TorrServer');
-        }
-    }
-
-    // --- Головна функція запуску ---
-    async function run(data) {
-        const movie = data.movie;
-        const query = `${movie.original_title || movie.original_name} ${movie.release_date ? movie.release_date.slice(0, 4) : ''}`;
-        
-        noty(`Пошук найкращого UA релізу...`);
-
-        let results = await getJackettResults(query);
-        
-        let scored = results
-            .map(item => ({ item, score: scoreRelease(item) }))
-            .filter(res => res.score > 0 && res.item.Size <= CONFIG.max_size && res.item.Seeders >= CONFIG.min_seeders)
-            .sort((a, b) => b.score - a.score);
-
-        if (scored.length > 0) {
-            const best = scored[0].item;
-            noty(`Знайдено: ${best.Title.slice(0, 30)}...`);
-            startPlay(best, movie);
-        } else {
-            noty('Українських релізів не знайдено');
-        }
-    }
-
-    // --- Інтерфейс (Кнопка) ---
+    // Додавання кнопки
     function addBtn() {
         Lampa.Listener.follow('full', function (e) {
             if (e.type === 'complite') {
-                const btn = $(`
-                    <div class="full-start__button selector playua-prime-btn">
-                        <svg width="16" height="16" viewBox="0 0 16 16" fill="white">
-                            <path d="M1 3a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V3zm7.039 3.399c0-.552-.446-1-1-1H5.433c-.552 0-1 .448-1 1v3.202c0 .552.448 1 1 1h1.606c.554 0 1-.448 1-1V6.399zM11.5 5.5h-1.306l-2.14 2.584L13.5 11h-1.428l-1.679-2.624-.615.7V11H8.59V5.001h1.187v2.686h.057L12.102 5z"/>
-                        </svg>
-                        <span>UA Prime</span>
-                    </div>
-                `);
-
-                btn.on('hover:enter', () => run(e.object));
+                var render = e.object.activity.render();
+                var container = render.find('.full-start__buttons, .full-start-new__buttons');
                 
-                const container = e.object.activity.render().find('.full-start__buttons, .full-start-new__buttons');
-                if (container.length) container.prepend(btn);
+                if (container.length && !container.find('.playua-prime-btn').length) {
+                    var btn = $(`
+                        <div class="full-start__button selector playua-prime-btn" style="background: #a37a00 !important; color: #fff !important;">
+                            <svg width="16" height="16" viewBox="0 0 16 16" fill="white" style="margin-right: 5px; vertical-align: middle;">
+                                <path d="M1 3a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V3zm7.039 3.399c0-.552-.446-1-1-1H5.433c-.552 0-1 .448-1 1v3.202c0 .552.448 1 1 1h1.606c.554 0 1-.448 1-1V6.399zM11.5 5.5h-1.306l-2.14 2.584L13.5 11h-1.428l-1.679-2.624-.615.7V11H8.59V5.001h1.187v2.686h.057L12.102 5z"/>
+                            </svg>
+                            <span>UA Prime</span>
+                        </div>
+                    `);
+
+                    btn.on('click', function() {
+                        UaDV.run(e.object);
+                    });
+
+                    container.prepend(btn);
+                    
+                    // Оновлюємо контролер, щоб кнопка стала доступною для фокусу пультом
+                    if (window.Lampa && Lampa.Controller) {
+                        Lampa.Controller.collectionSet(render);
+                    }
+                }
             }
         });
     }
 
-    // Запуск плагіна
     if (!window.uadv_prime_loaded) {
         window.uadv_prime_loaded = true;
         addBtn();
     }
 })();
-
