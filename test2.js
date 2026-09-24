1ae19a2d1154a11eb1858f4f0d36adf8df0dd634
crowley24 <unclezhenya26@gmail.com>
2026-09-24 20:50:51 +0300

    Fix typo in event type from 'complite' to 'complete'

--- test2.js
+++ test2.js
@@ -849,7 +849,7 @@ async function processImages(render, data, res) {
                 
     function attachLoader() {                
         Lampa.Listener.follow('full', (event) => {                
-            if (event.type === 'complite') {                
+            if (event.type === 'complete') {                
                 const data = event.data.movie;                
                 const render = event.object.activity.render();                
                 const cardRoot = render.find('.full-start-new.left-title');                
